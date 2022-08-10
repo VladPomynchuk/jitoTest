fe619e788a9425048d31edd1aaefbcc40cc23565
Vlad Pomynchuk <pomynchuk123@gmail.com>
2022-08-11 01:58:19 +0300

    fix react-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './components/GlobalStyle';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <GlobalStyle />
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
